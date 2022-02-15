364252947e179dfe9ca39202c0d5e55687df1b10
pdr3392 <pchenr1que1@gmail.com>
2022-02-15 02:04:54 -0300

    fixing subscription6

--- src/pages/api/subscribe.ts
+++ src/pages/api/subscribe.ts
@@ -1,24 +1,24 @@
-import { query as q } from "faunadb";
 import { NextApiRequest, NextApiResponse } from "next";
 import { getSession } from "next-auth/client";
 import { fauna } from "../../services/fauna";
+import { query as q } from "faunadb";
 import { stripe } from "../../services/stripe";
 
-type User = {
+interface UserProps {
   ref: {
     id: string;
   };
   data: {
     stripe_customer_id: string;
   };
-};
+}
 
-export default async (req: NextApiRequest, res: NextApiResponse) => {
+const subscribeFunction = async (req: NextApiRequest, res: NextApiResponse) => {
   if (req.method === "POST") {
     const session = await getSession({ req });
 
-    const user = await fauna.query<User>(
-      q.Get(q.Match(q.Index("user_by_email"), session.user.email))
+    const user = await fauna.query<UserProps>(
+      q.Get(q.Match(q.Index("user_by_email"), q.Casefold(session.user.email)))
     );
 
     let customerId = user.data.stripe_customer_id;
@@ -26,7 +26,6 @@ export default async (req: NextApiRequest, res: NextApiResponse) => {
     if (!customerId) {
       const stripeCustomer = await stripe.customers.create({
         email: session.user.email,
-        // metadata
       });
       await fauna.query(
         q.Update(q.Ref(q.Collection("users"), user.ref.id), {
@@ -35,6 +34,7 @@ export default async (req: NextApiRequest, res: NextApiResponse) => {
           },
         })
       );
+
       customerId = stripeCustomer.id;
     }
 
@@ -49,9 +49,11 @@ export default async (req: NextApiRequest, res: NextApiResponse) => {
       cancel_url: process.env.STRIPE_CANCEL_URL,
     });
 
-    return res.status(200).json({ sessionId: stripeCheckoutSession.id });
+    return res.status(200).json({ sessionId: stripeCheckoutSession });
   } else {
     res.setHeader("Allow", "POST");
     res.status(405).end("Method not allowed");
   }
 };
+
+export default subscribeFunction;
